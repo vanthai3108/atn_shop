38b783c39dbbb1671a772e60de2062560b34605d
Thai <vanthai3108@gmail.com>
2021-07-14 17:07:40 +0700

    edit register

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
-// $db_host = '54.236.43.60';
-$db_host = '52.6.114.59';
+$db_host = '54.236.43.60';
+// $db_host = '52.6.114.59';
 $db_user = 'thai';
 $db_pass = 'thai';
 $db_name = 'atn_shop';
